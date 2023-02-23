97761c8b38c3ba7e2c986a1eea7344b390a9f972
brenopinna <brenopinna1008@gmail.com>
2023-02-23 20:04:54 -0300

    update classes-dom-exercise

--- poo/classes-do-es6/classes-dom-exercicio/classes/Label.js
+++ poo/classes-do-es6/classes-dom-exercicio/classes/Label.js
@@ -1,8 +1,8 @@
 import Component from "./Component.js"
 
 class Label extends Component {
-  constructor(innerText, htmlFor, options, parent) {
-    super("label", parent, Object.assign({}, options, { innerText, htmlFor }))
+  constructor(textContext, htmlFor, options, parent) {
+    super("label", parent, Object.assign({}, options, { textContext, htmlFor }))
   }
 }
 
